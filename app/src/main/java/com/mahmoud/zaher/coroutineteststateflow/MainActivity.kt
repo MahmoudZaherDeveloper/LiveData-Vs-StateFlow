1a37c9513491ed560cbb9bdcd9a743309b9625ac
MahmoudZaherDeveloper <mahmoudzaher.developer@gmail.com>
2022-10-14 20:35:29 +0200

    state flow implementation

--- app/src/main/java/com/mahmoud/zaher/coroutineteststateflow/MainActivity.kt
+++ app/src/main/java/com/mahmoud/zaher/coroutineteststateflow/MainActivity.kt
@@ -2,13 +2,14 @@ package com.mahmoud.zaher.coroutineteststateflow
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
 import com.mahmoud.zaher.coroutineteststateflow.databinding.ActivityMainBinding
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
     private lateinit var timerViewModel: TimerViewModel
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         timerViewModel = ViewModelProvider(this).get(TimerViewModel::class.java)
         super.onCreate(savedInstanceState)
@@ -17,8 +18,11 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         timerViewModel.startTimer()
-        timerViewModel.timerLiveData.observe(this, Observer {
-            binding.timerTextView.text = it.toString()
-        })
+
+        lifecycleScope.launch {
+            timerViewModel.stateFlowTimer.collect {
+                binding.timerTextView.text = it.toString()
+            }
+        }
     }
 }
--- app/src/main/java/com/mahmoud/zaher/coroutineteststateflow/TimerViewModel.kt
+++ app/src/main/java/com/mahmoud/zaher/coroutineteststateflow/TimerViewModel.kt
@@ -1,24 +1,24 @@
 package com.mahmoud.zaher.coroutineteststateflow
 
 import android.util.Log
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.launch
 
 class TimerViewModel : ViewModel() {
-    private val _timerLiveData = MutableLiveData<Int>()
-    val timerLiveData: MutableLiveData<Int> = _timerLiveData
+    private val _stateFlowTimer = MutableStateFlow<Int>(0)
+    val stateFlowTimer: MutableStateFlow<Int> = _stateFlowTimer
 
     fun startTimer() {
         viewModelScope.launch {
-            val list = listOf(1, 1, 1, 2, 2, 3, 3, 3, 4, 5, 5, 6, 6, 7)
-            // for (i in 1..10) {
+            val list = listOf(1, 1, 2, 2, 3, 3, 3, 4, 5, 5, 6, 6, 7)
             for (i in list) {
-                _timerLiveData.value = i
+                //for (i in 1..10) {
+                _stateFlowTimer.emit(i)
                 delay(1000)
-                Log.d("TAG", "startTimer: $i ")
+                Log.d("TAG", "startTimer: $i")
             }
         }
     }
